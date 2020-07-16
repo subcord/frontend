ec1a4da3c9ece0fa730b52a0d3fd6df9dbd207d5
Joseph Banks <joseph@josephbanks.me>
2020-07-17 00:59:12 +0100

    :lipstick::bug: Fix top line of hero being covered by navbar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,28 +6,31 @@ import {faReddit} from '@fortawesome/free-brands-svg-icons';
 
 function Header() {
     return (
-        <div className="Header-bar">
-            <div className="Header">
-                <img src={logo} className="Header-image" height={70} alt="subcord logo" />
-                <h1 className="Header-text">
-                    SUBCORD
-                </h1>
-                <div className="Header-link-container">
-                    <a href="#" className="Header-link active-link">
-                        Home
-                    </a>
-                    <a href="#" className="Header-link">
-                        Reddit
-                    </a>
-                    <a href="#" className="Header-link">
-                        Panley Entertainment
-                    </a>
-                    <a href="#" className="Header-link reddit">
-                        <FontAwesomeIcon icon={faReddit} />
-                        Sign in with Reddit
-                    </a>
+        <div>
+            <div className="Header-bar">
+                <div className="Header">
+                    <img src={logo} className="Header-image" height={70} alt="subcord logo" />
+                    <h1 className="Header-text">
+                        SUBCORD
+                    </h1>
+                    <div className="Header-link-container">
+                        <a href="#" className="Header-link active-link">
+                            Home
+                        </a>
+                        <a href="#" className="Header-link">
+                            Reddit
+                        </a>
+                        <a href="#" className="Header-link">
+                            Panley Entertainment
+                        </a>
+                        <a href="#" className="Header-link reddit">
+                            <FontAwesomeIcon icon={faReddit} />
+                            Sign in with Reddit
+                        </a>
+                    </div>
                 </div>
             </div>
+            <div className="Header-content-block"></div>
         </div>
     );
 }
