134d27859bb6e7ae926bd73d8a7fe97241267c12
Joseph Banks <joseph@josephbanks.me>
2020-07-16 23:18:08 +0100

    :sparkles: Add Hero component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './components/header/Header';
+import Hero from './components/hero/Hero';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header/>
+      <Hero/>
     </div>
   );
 }
