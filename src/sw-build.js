60c080c5a8827b1952c54f164a7950babea67bf4
Joseph Banks <joseph@josephbanks.me>
2020-07-17 15:31:29 +0100

    :art: Code format of service worker files

--- src/sw-build.js
+++ src/sw-build.js
@@ -1,23 +1,23 @@
 const workboxBuild = require("workbox-build");
 
 const buildSW = () => {
-  return workboxBuild
-    .injectManifest({
-      swSrc: "src/sw-custom.js", // custom sw rule
+    return workboxBuild
+        .injectManifest({
+            swSrc: "src/sw-custom.js", // custom sw rule
 
-      swDest: "build/sw.js", // sw output file (auto-generated
+            swDest: "build/sw.js", // sw output file (auto-generated
 
-      globDirectory: "build",
+            globDirectory: "build",
 
-      globPatterns: ["**/*.{js,css,html,png,svg}"],
+            globPatterns: ["**/*.{js,css,html,png,svg}"],
 
-      maximumFileSizeToCacheInBytes: 5 * 1024 * 102,
-    })
-    .then((data) => {
-      data.warnings.forEach(console.warn);
-      console.info(`${data.count} files will be precached,
+            maximumFileSizeToCacheInBytes: 5 * 1024 * 102,
+        })
+        .then((data) => {
+            data.warnings.forEach(console.warn);
+            console.info(`${data.count} files will be precached,
                   totaling ${data.size / (1024 * 1024)} MBs.`);
-    });
+        });
 };
 
 buildSW();
--- src/sw-custom.js
+++ src/sw-custom.js
@@ -1,55 +1,55 @@
 if ("function" === typeof importScripts) {
-  importScripts(
-    "https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js"
-  );
-
-  if (workbox) {
-    console.log("Workbox is loaded");
-
-    self.addEventListener("install", (event) => {
-      self.skipWaiting();
-    });
-
-    workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-
-    workbox.routing.registerRoute(
-      new RegExp("https://fonts.(?:.googlepis|gstatic).com/(.*)"),
-      workbox.strategies.cacheFirst({
-        cacheName: "googleapis",
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 30,
-          }),
-        ],
-      })
+    importScripts(
+        "https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js"
     );
 
-    workbox.routing.registerRoute(
-      /\.(?:png|gif|jpg|jpeg|svg)$/,
-      workbox.strategies.cacheFirst({
-        cacheName: "images",
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 60,
-            maxAgeSeconds: 60 * 60, // 1 hour
-          }),
-        ],
-      })
-    );
+    if (workbox) {
+        console.log("Workbox is loaded");
 
-    workbox.routing.registerRoute(
-      /\.(?:js|css)$/,
-      workbox.strategies.staleWhileRevalidate({
-        cacheName: "static-resources",
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 60,
-            maxAgeSeconds: 60 * 60, // 1 hour
-          }),
-        ],
-      })
-    );
-  } else {
-    console.error("Workbox could not be loaded. No offline support");
-  }
+        self.addEventListener("install", (event) => {
+            self.skipWaiting();
+        });
+
+        workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+        workbox.routing.registerRoute(
+            new RegExp("https://fonts.(?:.googlepis|gstatic).com/(.*)"),
+            workbox.strategies.cacheFirst({
+                cacheName: "googleapis",
+                plugins: [
+                    new workbox.expiration.Plugin({
+                        maxEntries: 30,
+                    }),
+                ],
+            })
+        );
+
+        workbox.routing.registerRoute(
+            /\.(?:png|gif|jpg|jpeg|svg)$/,
+            workbox.strategies.cacheFirst({
+                cacheName: "images",
+                plugins: [
+                    new workbox.expiration.Plugin({
+                        maxEntries: 60,
+                        maxAgeSeconds: 60 * 60, // 1 hour
+                    }),
+                ],
+            })
+        );
+
+        workbox.routing.registerRoute(
+            /\.(?:js|css)$/,
+            workbox.strategies.staleWhileRevalidate({
+                cacheName: "static-resources",
+                plugins: [
+                    new workbox.expiration.Plugin({
+                        maxEntries: 60,
+                        maxAgeSeconds: 60 * 60, // 1 hour
+                    }),
+                ],
+            })
+        );
+    } else {
+        console.error("Workbox could not be loaded. No offline support");
+    }
 }
